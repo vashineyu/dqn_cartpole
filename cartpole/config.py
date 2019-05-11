609532c6896280e73d9e31a2c4e764245b1449ff
seanyu <r01227113@g.ntu.edu.tw>
2019-05-11 23:28:38 +0800

    sync

--- cartpole/config.py
+++ cartpole/config.py
@@ -3,6 +3,21 @@ from yacs.config import CfgNode as CN
 _C = CN()
 _C.SYSTEM = CN()
 _C.SYSTEM.DEVICES = []
+_C.SYSTEM.VIRTUAL_SCREEN = (400, 600)
+
+_C.AGENT = CN()
+_C.AGENT.NUM_EPISODE = 50
+_C.AGENT.NUM_MEMORY_CAPACITY = 1e+6
+_C.AGENT.BATCH_SIZE = 128
+_C.AGENT.GAMMA = 0.999
+_C.AGENT.EPS_START = 0.9
+_C.AGENT.EPS_END = 0.05
+_C.AGENT.EPS_DECAY = 200
+_C.AGENT.TARGET_UPDATE = 10
+
+_C.MODEL = CN()
+_C.MODEL.OPTIMIZER = "sgd"
+
 
 def get_cfg_defaults():
     return _C.clone()
--- cartpole/model.py
+++ cartpole/model.py
@@ -2,3 +2,37 @@
 Define model structure
 """
 import tensorflow as tf
+from tensorflow.python.keras import layers, models
+
+class DQN(models.Model):
+    def __init__(self, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = layers.Conv2D(filters=16, kernel_size=5, strides=2, padding="same")
+        self.conv2 = layers.Conv2D(filters=32, kernel_size=5, strides=2, padding="same")
+        self.conv3 = layers.Conv2D(filters=32, kernel_size=5, strides=2, padding="same")
+        self.bn1 = layers.BatchNormalization(axis=-1)
+        self.bn2 = layers.BatchNormalization(axis=-1)
+        self.bn3 = layers.BatchNormalization(axis=-1)
+        self.flat = layers.Flatten()
+
+        self.head = layers.Dense(units=outputs)
+
+    def call(self, x):
+        x = tf.convert_to_tensor(x, dtype=tf.float32)
+        x = tf.nn.relu(self.bn1(self.conv1(x)))
+        x = tf.nn.relu(self.bn2(self.conv2(x)))
+        x = tf.nn.relu(self.bn3(self.conv3(x)))
+        x = self.flat(x)
+
+        # use L1-loss, no need other activation for outputs
+        return self.head(x)
+
+if __name__ == "__main__":
+    import numpy as np
+    import os
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    model = DQN(2)
+
+    array = np.random.random((256, 256, 3))
+    out = model(array[np.newaxis, :,:,:])
+    model.summary()
