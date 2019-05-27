3a927c99979c59e3e5694216a60fd3aaf978b892
seanyu <r01227113@g.ntu.edu.tw>
2019-05-27 16:03:19 +0800

    update

--- run_torch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python run_torch.py SYSTEM.DEVICES "[3]" AGENT.CONV_MODE 0 AGENT.NUM_EPISODE 2000
---
+++ run_torch_dqn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run_torch_dqn.py SYSTEM.DEVICES "[3]" AGENT.CONV_MODE 0 AGENT.NUM_EPISODE 2000
